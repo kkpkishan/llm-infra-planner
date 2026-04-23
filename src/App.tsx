cdfee4325ef43a647a113ada5e31c0c91f08de4c
Kishan Khatrani <kishankhatrani@Kishans-MacBook-Air.local>
2026-04-23 10:12:15 +0530

    feat: implement GA consent mode v2 with EEA region defaults, URL passthrough, ads data redaction

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ function AppContent() {
       window.gtag('event', 'page_view', {
         page_path: location.pathname + location.search,
         page_title: document.title,
+        page_location: window.location.href,
       });
     }
   }, [location]);
